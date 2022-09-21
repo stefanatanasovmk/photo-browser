f335117aa5f7750813d41bfab9bf8369a7978f6f
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-09-21 20:34:59 +0300

    improved responsivness

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import InfoMsg from "./InfoMsg";
 import { useAppDispatch, useAppSelector } from "./redux/app/hooks";
@@ -13,18 +13,44 @@ export default function Image(): JSX.Element {
   const { id } = useParams<ParamsProps>();
   const { image, status } = useAppSelector((state) => state.images);
 
+  //Responsivness state
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 600px)").matches
+  );
   useEffect(() => {
     if (id !== undefined) {
       dispatch(getImage(id));
     }
+    window
+      .matchMedia("(min-width: 600px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
   }, [dispatch, id]);
+
+  const imageContainer: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    backgroundColor: "#FFFEFE",
+    margin: "20px",
+    height: "90%",
+    width: matches ? "600px" : "90%",
+    borderRadius: "20px",
+    boxShadow: "1px 2px 5px rgba(0,0,0,0.75)",
+  };
+
   return (
     <div style={imageContainer}>
       {status !== "error" ? (
         <>
           {status !== "loading" ? (
             <>
-              <img style={imageStyle} src={image.url} alt={image.title} />
+              <img
+                style={imageStyle}
+                src={image.url}
+                alt={image.title}
+                width="100%"
+              />
               <div style={imageText}>
                 <p>imageId: {image.id}</p>
                 <p>description/title: {image.title}</p>
@@ -46,18 +72,6 @@ export default function Image(): JSX.Element {
   );
 }
 
-const imageContainer: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-start",
-  alignItems: "flex-start",
-  backgroundColor: "#FFFEFE",
-  margin: "20px",
-  height: "90%",
-  width: "600px",
-  borderRadius: "20px",
-  boxShadow: "1px 2px 5px rgba(0,0,0,0.75)",
-};
 const imageStyle: React.CSSProperties = {
   borderTopLeftRadius: "20px",
   borderTopRightRadius: "20px",
